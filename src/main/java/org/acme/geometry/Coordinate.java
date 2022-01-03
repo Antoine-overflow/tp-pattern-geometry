da412219446e01ccfd11c3c44a5b120847e05121
Antoine-overflow <antoine.rainaud@ensg.eu>
2022-01-03 13:55:39 +0100

    0.1 - Coordinate (2D)

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,26 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
+
+    public Coordinate(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Coordinate(double xi, double yi){
+        this.x = xi;
+        this.y = yi;
+    } 
+
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
+
 
 }
+
