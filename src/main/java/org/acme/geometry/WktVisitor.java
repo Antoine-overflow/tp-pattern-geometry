0840a7328bc91a7526419c697cd2b00e4114dae0
antoine-overflow <antoine@DESKTOP-VKFREPH.localdomain>
2022-01-06 18:04:11 +0100

    0.10 - WktVisitor

---
+++ src/main/java/org/acme/geometry/WktVisitor.java
@@ -0,0 +1,39 @@
+package org.acme.geometry;
+
+public class WktVisitor implements GeometryVisitor {
+    private StringBuilder buffer;
+
+    public WktVisitor(){
+        this.buffer = new StringBuilder();
+    }
+    
+    public void visit(Point point){
+        if (point.isEmpty()) {
+            this.buffer.append("POINT EMPTY");
+        } else {
+            this.buffer.append("POINT("+ point.getCoordinate().getX() +" "+point.getCoordinate().getY()+")");
+        }
+    }
+
+    public void visit(LineString line){
+        if (line.isEmpty()) {
+            this.buffer.append("LINESTRING EMPTY");
+        } else {
+            String r = "LINESTRING(";
+            for(int i = 0 ; i < line.getNumPoints() ; i++){
+                r += line.getPointN(i).getCoordinate().getX() + " " + line.getPointN(i).getCoordinate().getY();
+                if (i != line.getNumPoints() - 1){
+                    r += ",";
+                }
+            }
+            r += ")";
+            this.buffer.append(r);
+        }
+    }
+
+    public String getResult(){
+        return this.buffer.toString();
+    };
+
+
+}
